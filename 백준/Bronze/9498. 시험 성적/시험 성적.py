d2d3442c63484d8477543341d6c0f8c72905debb
99millar <102401604+9-9millar@users.noreply.github.com>
2022-05-20 13:15:18 +0900

    [Bronze IV] Title: 시험 성적, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,14 @@
+tg = int(input())
+
+if 90 <= tg <=100:
+    print('A')
+elif 80 <= tg < 90:
+    print('B')
+elif 70 <= tg < 80:
+    print('C')
+elif 60 <= tg < 70:
+    print('D')
+elif 0<= tg < 60:
+    print('F')
+
+
